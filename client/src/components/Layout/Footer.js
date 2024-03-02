fd02e31da1465ac3412d5bbfd81ab4a9e3563168
Aahmad Saleem <aahmad.saleem95@gmail.com>
2024-03-02 22:27:24 +0500

    initial setup of the expense management system

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-light p-4">
+      <h5 className="text-center">All rights reserved &copy; HackiesTech</h5>
+    </div>
+  );
+};
+
+export default Footer;
