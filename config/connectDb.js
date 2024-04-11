df77714335cee3a94afbac4ff46a497a4524a4a9
Aahmad Saleem <aahmad.saleem95@gmail.com>
2024-04-11 21:26:19 +0500

    optimized code for logs

--- config/connectDb.js
+++ config/connectDb.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const colors = require("colors");
 const connectDb = async () => {
+  console.log("MONGO+_URL: ", process.env.MONGO_URL);
   try {
     await mongoose.connect(process.env.MONGO_URL);
     console.log(`Server Running on ${mongoose.connection.host}`.bgCyan.white);
